c4f10427f3b06e862d4fc3ff7a7ea574fb15840d
Camille Paquet <camille.paquet@yahoo.com>
2022-02-16 17:15:45 +0100

    refactor: 💡 amelioration du inputsearch

--- src/components/InputSearch.js
+++ src/components/InputSearch.js
@@ -1,23 +1,24 @@
 import useStyles from "./InputSearch.style";
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams,useHistory } from "react-router-dom";
 function InputSearch(props,ref) {
   const [params] = useSearchParams();
   const [value, setValue] = useState("");
 
-
-
   useEffect(() => {
     if (params.get('title')) {
       setValue(params.get('title').toLocaleUpperCase());
-      props.onAdd(value.toLocaleUpperCase());
     }
-  }, [params, value])
+  }, [params])
   
+  useEffect(() => {
+    props.onAdd(value.toLocaleUpperCase());
+    
+  }, [value])
+
 
   const onChange = (event) => {
     setValue(event.target.value.toLocaleUpperCase());
-    props.onAdd(event.target.value.toLocaleUpperCase());
   };
 
   return (

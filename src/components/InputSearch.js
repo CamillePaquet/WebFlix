135dbffe8c34aaa6afd9223abba8effe9f3b0109
Camille Paquet <camille.paquet@yahoo.com>
2022-02-17 10:52:54 +0100

    refactor: 💡 setsearchparams

--- src/components/InputSearch.js
+++ src/components/InputSearch.js
@@ -1,30 +1,30 @@
 import useStyles from "./InputSearch.style";
 import { useEffect, useState } from "react";
 import { useSearchParams, useParams } from "react-router-dom";
-function InputSearch(props,navigation) {
-  const [params] = useSearchParams();
+
+function InputSearch(props) {
+  const [searchParams,setSearchParams] = useSearchParams();
   const [value, setValue] = useState("");
+  const classes = useStyles();
 
   useEffect(() => {
-    if (params.has('title')) {
-      setValue(params.get('title').toLocaleUpperCase());
-    }
-  }, [params])
+    if (searchParams.has('title')) {
+      setValue(searchParams.get('title').toLocaleUpperCase());
+    } 
+  }, [searchParams])
   
   useEffect(() => {
     props.onAdd(value.toLocaleUpperCase());
+    
   }, [value])
-
-
 
   const onChange = (event) => {
     setValue(event.target.value.toLocaleUpperCase());
-    
-
+    setSearchParams({ title: event.target.value });
   };
 
   return (
-    <input
+    <input className={classes.inputSearch}
       type="text"
       value={value}
       onChange={onChange}
